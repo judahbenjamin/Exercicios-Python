8c5feb694f318701a581ba7af32919cb57e5d466
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-02-27 08:57:39 -0300

    Add files via upload

---
+++ Ex3.py
@@ -0,0 +1,28 @@
+#LOGICA E ALGORITMO DE PROGRAMACAO
+#PROF: MARCIO CLAY
+#ALUNO: JUDAH BENJAMIN
+#DATA: 27/02/2024
+# CONDICIONAL IF
+
+x = 10
+
+if x > 10 :
+    print("Maior")
+else :
+    print("Menor")
+    
+x = 15
+
+if x == 15 :
+    print("Ola, Mundo!")
+else :
+    print("Oi")
+    
+
+numero = int(input("Digite um numero:"))
+
+if numero > 0 :
+    print("O numero e positivo")
+else :
+    print("O numero e negativo")
+    
