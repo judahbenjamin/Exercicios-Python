142aa6d743c4c23158a3c13ed8295ab9d7b05ee7
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-03-13 11:01:41 -0300

    Add files via upload

---
+++ vetor.py
@@ -0,0 +1,58 @@
+#LÓGICA E ALGORITMO DE PROGRAMAÇÃO
+#PROF: MÁRCIO CLAY
+#DATA: 13/03/2024
+#ALUNO: JUDAH BENJAMIN
+#VETOR
+
+Nome = "Judah Benjamin"
+
+print(Nome[0])
+print(Nome[1])
+print(Nome[2])
+print(Nome[3])
+print(Nome[4])
+print(Nome[5])
+print(Nome[1:5])
+
+Frase = "No Sacrifice, no Victory"
+print(Frase[0])
+print(Frase[1])
+print(Frase[2])
+
+print(Frase[-5])
+print(Frase[-2])
+print(Frase[-3])
+print(Frase[-4])
+
+#MÉTODOS STRING
+
+# /N - muda para a próxima linha
+# LOWER - transforma em minúsculo
+#city = input("Digite uma cidade: \n").lower()
+#print(city)
+
+#UPPER - transforma em maiúsculo
+#city = input("Digite uma cidade: \n").upper()
+#print(city)
+
+# STRIP - separa as letras
+#nome = input("Digite um nome: \n").strip()
+#print(nome[0],nome[1],nome[2],nome[3],nome[4])
+
+#SPLIT - separa as palavras em parte
+#nome = input("Digite um nome: \n").split()
+#print(nome)
+
+print("========== DESAFIO ===========")
+
+mensagem = "Tamo junto"
+
+print(mensagem[0],mensagem[3])
+print(mensagem[0:5])
+print(mensagem[5:10])
+
+print("=============== TESTANDO OS MÉTODOS =================")
+
+#Ler o nome completo da pessoa
+Name = input("Digite seu nome: \n").strip()
+print(Name)
