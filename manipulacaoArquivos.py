5125a6d314f53856093176e4ee6bb216976e8778
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-05-07 08:57:05 -0300

    Add files via upload

---
+++ manipulacaoArquivos.py
@@ -0,0 +1,56 @@
+#LÓGICA DE PROGRAMAÇÃO E ALGORITMO
+#DATA: 07/05/2024
+#JUDAH BENJAMIN
+
+# with open("registro.txt","a") as gravar:
+#     gravar.write("Ola,mundo!")
+#     gravar.write("Hello")
+    
+# with open("codigo.txt","r") as ler:
+#     x = ler.read()
+#     print(x)
+
+# with open("texto.txt","a") as gravar:
+#     gravar.write(f"\nBELEZA")
+    
+# with open("texto.txt","r") as ler:
+#     x = ler.read()
+#     print(x)
+    
+# # criar = open("Judah.txt","x")
+
+# with open("Judah.txt","w") as gravando:
+#     gravando.write("Bom dia! Vamos simbora")
+#     gravando.write(f"\nVamos simbora")
+#     gravando.write(f"\nComida = LASANHA")
+    
+# with open("Judah.txt","r") as ler:
+#     l = ler.read()
+#     print(l)
+    
+# Abrir = open("Judah.txt","rt")w
+
+# l = open("demofile.txt","a")
+# l.write(f"\nNow i am Master!")
+# l.close()
+
+"""
+EXERCICIO
+
+1. Ler um arquivo.txt
+2. Encontrar código esperado
+3. Se o codigo correto escrever em um arquivo de registro.txt ("codigo correto")    
+"""
+
+# with open("meuArquivo.txt","a") as gravando:
+#     gravando.write(f"\nBom dia! Hoje e terca e quero cafe!")
+#     gravando.close()
+    
+# with open("meuArquivo.txt","r") as leitura:
+#     l = leitura.read()
+#     print(l)
+
+
+
+
+    
