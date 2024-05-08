96ea07e402338c751e5d7d5e6e445809b918a2d9
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-05-08 11:00:38 -0300

    Add files via upload

---
+++ openFile.py
@@ -0,0 +1,32 @@
+#LÓGICA DE PROGRAMAÇÃO E ALGORITMO
+#PROFESSOR: MÁRCIO CLAY
+#DATA: 08/05/2024
+#ALUNO: JUDAH BENJAMIN
+
+# def saudacao (nome):
+#     print(f"Bom dia, {nome}")
+
+# import openFile
+# openFile.saudacao ("Pedro")
+
+# criar = open("Arquivo.txt","a")
+
+# contador = 0
+
+# while contador < 10:
+#     with open("Arquivo.txt","a") as gravar:
+#         gravar.write(f"\nBom dia! Quarta feira, data 08/05/2024, quero café!")
+#         gravar.write(f"\nVamos estudar!")
+#         # gravar.write(f"\n1,2,3,4,5")
+#     # gravar.close()
+#     contador += 1
+           
+# with open("Arquivo.txt","r") as leitura:
+#     l = leitura.read()
+#     print(l)
+
+# leitura = open("Judah.txt","r")
+# l = leitura.read()
+# print(l)
+   
+
