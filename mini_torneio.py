e55cd967efed71d1e95a9ee4c630fe0fa2c728af
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-06-04 08:57:24 -0300

    Update mini_torneio.py

--- mini_torneio.py
+++ mini_torneio.py
@@ -1,5 +1,5 @@
 #LÓGICA DE PROGRAMAÇÃO MINI TORNEIO
-#DATA: 03/06/2024
+#DATA: 04/06/2024
 
 """
 1. Ler um 5 números e verificar se ele é par ou ímpar.
@@ -52,10 +52,40 @@ Entrada: Leitura de 10 números
 Saída: Exemplo( A média = 20, o maior = 9 e o menor= 3)    
 """
 
-cont = 0
+# n = []
+# cont = 0
 
-while cont < 10:
-    numero = int(input('Digite um numero: '))
-    cont += 1
-print('-='*30)
+# while cont < 10:
+#     numero = int(input('Digite um numero: '))
+#     n.append(numero)
+#     cont += 1
+# print('-='*30)
 
+# media = sum(n)/len(n)
+
+# print(f'A media = {media}, o maior = {max(n)} e o menor = {min(n)}')
+
+"""
+4. Faça um algoritmo que leia um conjunto de números (X) e imprima sua 
+soma (Soma) e sua média (Media). Admita que o valor 50 é utilizado 
+como sentinela para fim de leitura.  
+Entrada: Leitura de 1 a 50 números  
+Saída: Exemplo(Soma=6 Media=2)  
+"""
+
+# n = []
+# cont = 0
+# quant = int(input('Digite a quantidade de numeros: '))
+    
+# while cont < quant:
+#     num = int(input('Digite o numero: '))
+#     if num == 50:
+#         print('PROGRAMA ENCERRADO...')
+#         media = False
+#         break
+#     else:
+#         n.append(num)
+#         cont += 1
+#         media = sum(n)/len(n)
+
+# print(f'Soma = {sum(n)}, Media = {media}')
