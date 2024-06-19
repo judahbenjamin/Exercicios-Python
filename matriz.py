3e2486647676fa06ffb1cb1e308174b3c32a28cb
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-06-19 11:01:15 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,97 @@
+# # matriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+# # # print(matriz)
+# # for linha in matriz:
+# #     print(linha)
+# #     # for coluna in linha:
+# #     #     print(coluna)
+
+# # print('-='*30)
+# # print("ACESSANDO OS ELEMENTOS DA MATRIZ:")
+# # print(matriz[0][0])
+# # print(matriz[1][0])
+# # print(matriz[2][0])
+# # print()
+# # print(matriz[0][1])
+# # print(matriz[1][1])
+# # print(matriz[2][1])
+# # print()
+# # print(matriz[0][2])
+# # print(matriz[1][2])
+# # print(matriz[2][2])
+
+# #ATENÇÃO -----> matriz [linha][coluna]
+
+# # nomes = [['Judah','Linda Ines'],['Adenilton','Joao']]
+
+# # for linha in nomes:
+# #     print(linha)
+
+# # if linha == nomes[0][0]:
+# #     print(linha)
+# # print(nomes[0][0])
+# # print(nomes[1][0])
+
+# #EXERCÍCIO
+
+# # teste_1 = []
+# # teste_2 = []
+# # teste_3 = []
+# teste = []
+# matriz = [teste,teste,teste]
+
+# q = 0
+# pergunta = False
+
+# print('<       TESTE MATRIZ        >')
+
+# def AddMatriz (teste):
+#     teste = int(input("QUANTOS NUMEROS? "))
+#     matriz.append(teste)
+#     return teste
+
+# AddMatriz(teste)
+# print(teste)
+
+# # # pergunta = input('Deseja iniciar o programa? ')
+
+# # # if pergunta == "S":
+# # #     print('ok')
+# # # else:
+# # #     print('Nao')
+# # # while q < 3:
+
+# # #     print('PRIMEIRO VETOR:')
+    
+# # #     quant = int(input("QUANTOS NUMEROS? "))
+    
+# # #     for q in range(quant):
+# # #         num = int(input("NUMERO: "))
+# # #         teste_1.append(num)
+# # #         q += 1
+        
+# # # resposta_2 = False
+# # # resposta_2 = bool(input('Deseja continuar a adicionar no segundo vetor? (S/N) '))
+
+# # # if resposta_2 == True :
+# # #     quant_2 = int(input("QUANTOS NUMEROS? "))
+    
+# # #     while q2 < quant_2:
+# # #         num = int(input("NUMERO: "))
+# # #         teste_2.append(num)
+# # #         q2 += 1
+        
+        
+# # # resposta_3 = False
+# # # resposta_3 = bool(input('Deseja continuar a adicionar no terceiro vetor? (S/N) '))
+
+# # # if resposta_3 == True:
+# # #     quant_3 = int(input("QUANTOS NUMEROS? "))
+    
+# # #     while q3 < quant:
+# # #         num = int(input("NUMERO: "))
+# # #         teste_3.append(num)
+# # #         q3 += 1                       
+
+# # print(matriz)
+
