74511f01819f6784bb64d115052552a709581491
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-06-25 08:59:46 -0300

    Add files via upload

---
+++ exemplo_matriz.py
@@ -0,0 +1,16 @@
+notas = [[10,9,7],
+         [8,5,9],
+         [5,10,2]]
+
+print(notas[0][0])
+print(notas[1][0])
+print(notas[2][0])
+print(notas[0][1])
+print(notas[1][1])
+print(notas[2][1])
+print(notas[0][2])
+print(notas[1][2])
+print(notas[2][2])
+
+media = (notas[0][0] + notas[1][0] + notas[2][0])/3
+print(f'MEDIA: {media:.2f}')
