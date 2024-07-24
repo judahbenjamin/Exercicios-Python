c3df529693ddb92dce14839247507089faa4110b
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-07-23 22:50:55 -0300

    Add files via upload

--- revisao.py
+++ revisao.py
@@ -1,31 +1,46 @@
-"""
-Faça um programa que faça 5 perguntas para uma pessoa sobre um crime.
-As perguntas são:
-"Telefonou para a vítima?"
-"Esteve no local do crime?"
-"Mora perto da vítima?"
-"Devia para a vítima?"
-"Já trabalhou com a vítima?"
-O programa deve no final emitir uma classificação sobre a participação
-da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela
-deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como
-"Assassino". Caso contrário, ele será classificado como "Inocente". 
- """
+#REVISAO DE ALGORITMOS
+
+# 1 – Faça um programa que leia 10 números e informe:
+# a)	A soma destes números;
+# b)	A média destes números;
+# c)	O maior número; e
+# d)	O menor número.
+
+# numeros = [10,2,4,55,3,90,11,22,5,12]
+# print(f'Lista dos numeros: {numeros}')
+
+# soma = sum(numeros)
+# print(f'A soma dos numeros: {soma}')
+
+# media = sum(numeros)/len(numeros)
+# print(f'A media dos numeros: {media}')
+
+# maior = max(numeros)
+# print(f'O maior numero: {maior}')
+
+# menor = min(numeros)
+# print(f'O menor numero: {menor}')
+
+# 2 – Faça um programa que leia o nome, idade e sexo de várias pessoas até que o nome digitado seja “FIM” e informe:
+# a)	O nome e a idade da pessoa mais velha;
+# b)	O nome e a idade da pessoa mais nova;
+# c)	Quantas pessoas eram do sexo masculino;
+# d)	Quantas pessoas eram do sexo feminino; e
+# e)	A quantidade de pessoas digitadas e a média de idade.
 
 
-pergunta_1 = int(input('Telefonou para a vitima? [S = 1, N = 0] '))
-pergunta_2 = int(input('Esteve no local do crime? [S = 1, N = 0] '))
-pergunta_3 = int(input('Mora perto da vitima? [S = 1, N = 0] '))
-pergunta_4 = int(input('Devia para a vitima? [S = 1, N = 0] '))
-pergunta_5 = int(input('Ja trabalhou com a vitima? [S = 1, N = 0] '))
-
-resultado = pergunta_1 + pergunta_2 + pergunta_3 + pergunta_4 + pergunta_5
-
-if resultado == 2:
-    print('SUSPEITA')
-elif resultado >= 3 and resultado <= 4:
-    print('CUMPLICE')
-elif resultado == 5:
-    print('ASSASSINO')
-else:
-    print('INOCENTE')
+while True:
+    nome = input('NOME: ')
+    idade = int(input('IDADE: '))
+    sexo = input('SEXO: ')
+    print('='*30) 
+    
+    if nome == 'fim':
+        break
+    
+# 3 – Faça um programa que leia o número e o peso de um boi, a leitura encerra quando o número digitado for 0 (zero) e informe:
+# a)	Quantos bois foram digitados;
+# b)	Qual o total de peso dos bois;
+# c)	Qual a média de peso dos bois;
+# d)	Qual o número e o peso do boi mais pesado; e
+# e)	Qual o número e o peso do boi mais leve.
