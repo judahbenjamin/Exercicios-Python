64986fc06550427d1709c0dbd3f3e6572cb2e8cf
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-08-27 11:16:50 -0300

    Update list.py

--- list.py
+++ list.py
@@ -5,6 +5,7 @@ while True:
     print("####             MENU            ####")
     print("Inserir Dados Pessoa            < 1 >")
     print("Listar Pessoas                  < 2 >")
+    print("Visualizacao resumida           < 3 >")
     print("Sair                            < 9 >")
     opcao = input("Digite a opcao: ")
 
@@ -32,13 +33,21 @@ while True:
         listaDadoPessoa.append(salario)
 
         quant_filhos = int(input("Digite quantidade de filhos: "))
+        listaDadoPessoa.append(quant_filhos)
 
         ListaGeralPessoas.append(listaDadoPessoa)
 
     if opcao == "2":
         print("\n\n#### Lista dos dados inseridos   ###")
         for dadosUmaPessoa in ListaGeralPessoas:
-            nome, telefone, cpf, salario = dadosUmaPessoa
+            nome, telefone, cpf, rg, email, salario, quant_filhos = dadosUmaPessoa
+
+        print(ListaGeralPessoas)
+
+    if opcao == "3":
+        print("\n\n#### Visualizacao resumida   ###")
+        for dadosUmaPessoa in ListaGeralPessoas:
+            nome, cpf = dadosUmaPessoa
 
     if opcao == "9":
         print("PROGRAMA ENCERRADO...")
