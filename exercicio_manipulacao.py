0b13dcd488a275dd23cafb1fe73097e394712ec1
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-10-01 11:18:17 -0300

    Update exercicio_manipulacao.py

--- exercicio_manipulacao.py
+++ exercicio_manipulacao.py
@@ -12,43 +12,83 @@ Sair do programa( e gravar os dados que estão contidos na lista de dados)
 Atenção: O programa deve carregar os dados contidos no arquivo de "memória" ao ser inicializado. 
 """
 
-dados_pessoa = []
+# dados_pessoa = []
 
-def gravando_em_arquivo(nome,telefone,email,cpf):
-    with open('arquivo.txt','w') as arquivo:
-        arquivo.write("\n"+nome)
-        arquivo.write("\n"+telefone)
-        arquivo.write("\n"+email)
-        arquivo.write("\n"+cpf)
+# def gravando_em_arquivo(nome,telefone,email,cpf):
+#     with open('arquivo.txt','w') as arquivo:
+#         arquivo.write("\n"+nome)
+#         arquivo.write("\n"+telefone)
+#         arquivo.write("\n"+email)
+#         arquivo.write("\n"+cpf)
 
-def ler_arquivos():
-    with open('arquivo.txt','r') as arquivo:
-        conteudo = arquivo.read()
-        return conteudo
+# def ler_arquivos():
+#     with open('arquivo.txt','r') as arquivo:
+#         conteudo = arquivo.read()
+#         return conteudo
     
-dado_programa = ler_arquivos()
+# dado_programa = ler_arquivos()
 
-while True:
+# while True:
 
-    print("========= MENU =========")
-    print("Insere dados --------- 1")
-    print("Exibe dados ---------- 2")
-    print("Sair do programa ----- 3")
-    opcao = int(input("Digite uma opcao: "))
+#     print("========= MENU =========")
+#     print("Insere dados --------- 1")
+#     print("Exibe dados ---------- 2")
+#     print("Sair do programa ----- 3")
+#     opcao = int(input("Digite uma opcao: "))
 
-    if opcao == 1:
-        dados_pessoa = {}
-        nome = input("Nome da pessoa: ")
-        telefone = input("Telefone da pessoa: ")
-        email = input("E-mail da pessoa: ")
-        cpf = input("CPF da pessoa: ")
-        print()
-        gravando_em_arquivo(nome,telefone,email,cpf)
+#     if opcao == 1:
+#         dados_pessoa = {}
+#         nome = input("Nome da pessoa: ")
+#         telefone = input("Telefone da pessoa: ")
+#         email = input("E-mail da pessoa: ")
+#         cpf = input("CPF da pessoa: ")
+#         print()
+#         gravando_em_arquivo(nome,telefone,email,cpf)
 
-    elif opcao == 2:
-        print(ler_arquivos())
-        print()
+#     elif opcao == 2:
+#         print(ler_arquivos())
+#         print()
 
-    elif opcao == 3:
-        print("PROGRAMA FINALIZADO...")
-        break
+#     elif opcao == 3:
+#         print("PROGRAMA FINALIZADO...")
+#         break
+
+import json
+
+# Dicionário de exemplo
+
+cont = 0
+quant = int(input("Digite quantidade de vezes? "))
+
+for cont in range(cont):
+
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    cidade = input("Cidade: ")
+
+    pessoa = {
+        'nome': nome,
+        'idade': idade,
+        'cidade': cidade
+    }
+
+#PROCESSO DE GRAVAÇÃO DE DICIONÁRIO EM ARQUIVO.
+#
+# Grava o dicionário em um arquivo chamado 'dicionario.json'
+with open('arquivo.txt', 'w') as arquivo:
+    json.dump(pessoa, arquivo)
+    #Faz a união do dicionário com o objeto arquivo.
+
+
+#PROCESSO DE LEITURA EM ARQUIVO DE UM DICIONÁRIO.
+#
+# Inicializa um dicionário vazio para guardar os dados recebidos.
+dadosRecebidos = {}
+
+# Abre o arquivo 'dicionario.json' para leitura
+with open('arquivo.txt', 'r') as arquivo:
+    # Carrega os dados do arquivo para o dicionario_lido
+    dicionario_lido = json.load(arquivo)
+
+# Exibe o dicionário lido do arquivo
+print(dicionario_lido)
