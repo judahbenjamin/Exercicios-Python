dea2251344c6205d16c255d2035983b0db0545ea
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-10-02 17:38:31 -0300

    Update listas_manipulacao.py

--- listas_manipulacao.py
+++ listas_manipulacao.py
@@ -13,26 +13,57 @@ seguintes opções:
     arquivo para continuar o cadastro.
 """
 
-# def gravaDado_emArquivo(dadoRecebido):
-#     with open('arquivo.txt', 'w') as arquivo:
-#         arquivo.write("\n"+dadoRecebido)
+# import json
 
-# def leDados_emArquivo():
-#     with open('arquivo.txt', 'r') as arquivo:
-#         conteudo = arquivo.read()
-#         print(conteudo)
+# nomes = []
 
+# def adicionar_nomes():
+#     lista_nomes = {}
+    
+#     print(30*"=-")
+
+#     nome = input("Digite um nome: ")
+
+#     print(30*"=-")
+
+#     lista_nomes["Nome"] = nome
+    
+#     nomes.append(lista_nomes)
+
+# def listar_nomes():
+#   print()
+#   print("Listagem dos nomes cadastrados:")
+#   for umNome in nomes:
+#       print (umNome)
+
+# def gravar_dados():
+#     with open("arquivo1.json","w") as arquivo:
+#         json.dump(nomes,arquivo)
+
+#     with open("arquivo1.json","r") as arquivo:
+#         dados_lidos = json.load(arquivo)
+        
 # while True:
-#     print("Inserir dados -- 1")
-#     print("Exibir dados -- 2")
-#     opcao = input("Digite uma opcao: ")
+#     print("\n--- Menu ---")
+#     print("1. Adicionar nomes")
+#     print("2. Listar nomes")
+#     print("3. Sair")
+#     opcao = input("Escolha uma opção: ")
+#     print()
 
-#     if opcao == "1":
-#         dadoRecebido = input("Digite um nome: ")
-#         gravaDado_emArquivo(dadoRecebido)
+#     if opcao =='1':
+#         adicionar_nomes()
 
-#     elif opcao == "2":
-#         leDados_emArquivo()
+#     elif opcao == '2':
+#         listar_nomes()
+
+#     elif opcao == '3':
+#         gravar_dados()
+#         print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
+#         print("PROGRAMA FINALIZADO...")
+#         break
+#     else:
+#         print("Opção inválida.")
 
 
 """
@@ -46,6 +77,65 @@ opções:
     arquivo e, ao ser inicializado novamente, o programa deverá carregar os
     dados do arquivo para continuar o cadastro.
 """
+
+# import json
+
+# dados_pessoas = []
+
+# def adicionar_pessoas():
+#     lista_pessoas = {}
+    
+#     print(30*"=-")
+
+#     nome = input("Digite o nome: ")
+#     cpf = input("Digite o cpf: ")
+#     rg = input("Digite o rg: ")
+#     email = input("Digite o e-mail: ")
+
+#     print(30*"=-")
+
+#     lista_pessoas["Nome"] = nome
+#     lista_pessoas["CPF"] = cpf
+#     lista_pessoas["RG"] = rg
+#     lista_pessoas["E-mail"] = email
+    
+#     dados_pessoas.append(lista_pessoas)
+
+# def listar_dados():
+#   print()
+#   print("Listagem das pessoas cadastradas:")
+#   for dado in dados_pessoas:
+#       print (dado)
+
+# def gravar_dados():
+#     with open("arquivo2.json","w") as arquivo:
+#         json.dump(dados_pessoas,arquivo)
+
+#     with open("arquivo2.json","r") as arquivo:
+#         dados_lidos = json.load(arquivo)
+        
+# while True:
+#     print("\n--- Menu ---")
+#     print("1. Adicionar pessoas")
+#     print("2. Listar pessoas")
+#     print("3. Sair")
+#     opcao = input("Escolha uma opção: ")
+#     print()
+
+#     if opcao =='1':
+#         adicionar_pessoas()
+
+#     elif opcao == '2':
+#         listar_dados()
+
+#     elif opcao == '3':
+#         gravar_dados()
+#         print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
+#         print("PROGRAMA FINALIZADO...")
+#         break
+#     else:
+#         print("Opção inválida.")
+
 
 """
 3. Crie um script em Python que deverá permitir ao usuário cadastrar os dados
