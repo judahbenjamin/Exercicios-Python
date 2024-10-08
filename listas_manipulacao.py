3e94a1f6d791fa49e2c428cc68274cac0a7d4ccb
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-10-08 18:47:06 -0300

    Update listas_manipulacao.py

--- listas_manipulacao.py
+++ listas_manipulacao.py
@@ -150,89 +150,97 @@ apresentar as seguintes características/ funcionalidades:
     dados do arquivo para continuar o cadastro.
 """
 
-import json
+# import json
 
-dados_pessoas = []
+# dados_pessoas = []
+# dados_pessoas2 = []
 
-def adicionar_pessoas():
-    lista_pessoas = {}
+# def adicionar_pessoas():
+#     lista_pessoas = {}
     
-    print(30*"=-")
+#     print(30*"=-")
 
-    nome = input("Digite o nome: ")
-    cpf = input("Digite o cpf: ")
-    rg = input("Digite o rg: ")
-    email = input("Digite o e-mail: ")
-    endereco = input("Digite o endereco: ")
+#     nome = input("Digite o nome: ")
+#     cpf = input("Digite o cpf: ")
+#     rg = input("Digite o rg: ")
+#     email = input("Digite o e-mail: ")
+#     endereco = input("Digite o endereco: ")
 
-    print(30*"=-")
+#     print(30*"=-")
 
-    lista_pessoas["Nome"] = nome
-    lista_pessoas["CPF"] = cpf
-    lista_pessoas["RG"] = rg
-    lista_pessoas["E-mail"] = email
-    lista_pessoas["Endereco"] = endereco
+#     lista_pessoas["Nome"] = nome
+#     lista_pessoas["CPF"] = cpf
+#     lista_pessoas["RG"] = rg
+#     lista_pessoas["E-mail"] = email
+#     lista_pessoas["Endereco"] = endereco
     
-    dados_pessoas.append(lista_pessoas)
+#     dados_pessoas.append(lista_pessoas)
 
-def listar_dados():
-  print()
-  print("Listagem das pessoas cadastradas:")
-  for dado in dados_pessoas:
-      print (dado)
+# def listar_dados():
+#   print()
+#   print("Listagem das pessoas cadastradas:")
+#   for dado in dados_pessoas:
+#       print (dado)
 
-def buscar_dados():
-        b = input("Digite o CPF da pessoa: ")
+# # def buscar_dados():
+# #         b = input("Digite o CPF da pessoa: ")
 
-        #fazer um for aninhado
-        for b in dados_pessoas:
-            for b in dados_pessoas:
-                detalhes = dados_pessoas[b]
-                print(f"Nome: {detalhes["Nome"]}")
-                print(f"CPF: {detalhes["CPF"]}")
-                print(f"RG: {detalhes["RG"]}")
-                print(f"E-mail: {detalhes["E-mail"]}")
-                print(f"Endereco: {detalhes["Endereco"]}")
-        else:
-            print(f"A pessoa com o nome {b} não foi encontrada.")
+# #         #fazer um for aninhado
+# #         for b in dados_pessoas:
+# #             for b in dados_pessoas:
+# #                 detalhes = dados_pessoas[b]
+# #                 print(f"Nome: {detalhes["Nome"]}")
+# #                 print(f"CPF: {detalhes["CPF"]}")
+# #                 print(f"RG: {detalhes["RG"]}")
+# #                 print(f"E-mail: {detalhes["E-mail"]}")
+# #                 print(f"Endereco: {detalhes["Endereco"]}")
+# #         else:
+# #             print(f"A pessoa com o nome {b} não foi encontrada.")
         
-def listar_dados():
-  print()
-  print("Listagem das pessoas cadastradas:")
-  for dado in dados_pessoas:
-      print (dado)
+# def listar_dados():
+#   print()
+#   print("Listagem das pessoas cadastradas:")
+#   for dado in dados_pessoas:
+#       print (dado)
 
 
-# ler dados do arquivo dados1.txt e gravar estes dados na lista dados_pessoas
+# # ler dados do arquivo dados1.txt e gravar estes dados na lista dados_pessoas
+# def gravar_dados():
+#     with open("arquivo1.json","w") as arquivo:
+#         json.dump(dados_pessoas,arquivo)
 
+#     with open("arquivo1.json","r") as arquivo:
+#         dados_pessoas2 = arquivo.readline(dados_pessoas)
+#         print(dados_pessoas2)
     
-while True:
-    print("\n--- Menu ---")
-    print("1. Adicionar pessoas")
-    print("2. Buscar dados")
-    print("3. Listar pessoas")
-    print("4. Sair")
-    opcao = input("Escolha uma opção: ")
-    print()
+# while True:
+#     print("\n--- Menu ---")
+#     print("1. Adicionar pessoas")
+#     print("2. Buscar dados")
+#     print("3. Listar pessoas")
+#     print("4. Sair")
+#     opcao = input("Escolha uma opção: ")
+#     print()
 
-    if opcao =='1':
-        adicionar_pessoas()
+#     if opcao =='1':
+#         adicionar_pessoas()
 
-    elif opcao == '2':
-        buscar_dados()
+#     elif opcao == '2':
+#         buscar_dados()
 
-    elif opcao == '3':
-        listar_dados()
+#     elif opcao == '3':
+#         listar_dados()
 
-    elif opcao == '4':
-        print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
-        print("PROGRAMA FINALIZADO...")
+#     elif opcao == '4':
+#         gravar_dados()
+#         print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
+#         print("PROGRAMA FINALIZADO...")
 
-        #acionar função para gravar dados da lista em um arquivo de texto, exp: dados1.txt
+#         #acionar função para gravar dados da lista em um arquivo de texto, exp: dados1.txt
 
-        break
-    else:
-        print("Opção inválida.")
+#         break
+#     else:
+#         print("Opção inválida.")
 
 
 """
