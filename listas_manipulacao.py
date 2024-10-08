2b08b0e8e72c2ca7f2771f9bcdef7e105962bfac
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-10-08 11:21:44 -0300

    Update listas_manipulacao.py

--- listas_manipulacao.py
+++ listas_manipulacao.py
@@ -150,6 +150,91 @@ apresentar as seguintes características/ funcionalidades:
     dados do arquivo para continuar o cadastro.
 """
 
+import json
+
+dados_pessoas = []
+
+def adicionar_pessoas():
+    lista_pessoas = {}
+    
+    print(30*"=-")
+
+    nome = input("Digite o nome: ")
+    cpf = input("Digite o cpf: ")
+    rg = input("Digite o rg: ")
+    email = input("Digite o e-mail: ")
+    endereco = input("Digite o endereco: ")
+
+    print(30*"=-")
+
+    lista_pessoas["Nome"] = nome
+    lista_pessoas["CPF"] = cpf
+    lista_pessoas["RG"] = rg
+    lista_pessoas["E-mail"] = email
+    lista_pessoas["Endereco"] = endereco
+    
+    dados_pessoas.append(lista_pessoas)
+
+def listar_dados():
+  print()
+  print("Listagem das pessoas cadastradas:")
+  for dado in dados_pessoas:
+      print (dado)
+
+def buscar_dados():
+        b = input("Digite o CPF da pessoa: ")
+
+        #fazer um for aninhado
+        for b in dados_pessoas:
+            for b in dados_pessoas:
+                detalhes = dados_pessoas[b]
+                print(f"Nome: {detalhes["Nome"]}")
+                print(f"CPF: {detalhes["CPF"]}")
+                print(f"RG: {detalhes["RG"]}")
+                print(f"E-mail: {detalhes["E-mail"]}")
+                print(f"Endereco: {detalhes["Endereco"]}")
+        else:
+            print(f"A pessoa com o nome {b} não foi encontrada.")
+        
+def listar_dados():
+  print()
+  print("Listagem das pessoas cadastradas:")
+  for dado in dados_pessoas:
+      print (dado)
+
+
+# ler dados do arquivo dados1.txt e gravar estes dados na lista dados_pessoas
+
+    
+while True:
+    print("\n--- Menu ---")
+    print("1. Adicionar pessoas")
+    print("2. Buscar dados")
+    print("3. Listar pessoas")
+    print("4. Sair")
+    opcao = input("Escolha uma opção: ")
+    print()
+
+    if opcao =='1':
+        adicionar_pessoas()
+
+    elif opcao == '2':
+        buscar_dados()
+
+    elif opcao == '3':
+        listar_dados()
+
+    elif opcao == '4':
+        print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
+        print("PROGRAMA FINALIZADO...")
+
+        #acionar função para gravar dados da lista em um arquivo de texto, exp: dados1.txt
+
+        break
+    else:
+        print("Opção inválida.")
+
+
 """
 4. Crie um script em Python que deverá permitir ao usuário cadastrar os dados
 de "n" pessoas (nome, CPF, RG, e-mail, endereço e diversos telefones). O
@@ -164,3 +249,86 @@ programa deverá apresentar as seguintes características/funcionalidades:
     arquivo e, ao ser inicializado novamente, deverá carregar os dados do
     arquivo para continuar o cadastro.
 """
+
+# import json
+
+# dados_pessoas = []
+
+# def adicionar_pessoas():
+#     lista_pessoas = {}
+    
+#     print(30*"=-")
+
+#     nome = input("Digite o nome: ")
+#     cpf = input("Digite o cpf: ")
+#     rg = input("Digite o rg: ")
+#     email = input("Digite o e-mail: ")
+#     endereco = input("Digite o endereco: ")
+#     telefone = input("Digite o telefone: ")
+
+#     print(30*"=-")
+
+#     lista_pessoas["Nome"] = nome
+#     lista_pessoas["CPF"] = cpf
+#     lista_pessoas["RG"] = rg
+#     lista_pessoas["E-mail"] = email
+#     lista_pessoas["Endereco"] = endereco
+#     lista_pessoas["Telefone"] = telefone
+
+    
+#     dados_pessoas.append(lista_pessoas)
+
+# def listar_dados():
+#   print()
+#   print("Listagem das pessoas cadastradas:")
+#   for dado in dados_pessoas:
+#       print (dado)
+
+# def buscar_dados():
+#         busca = input("Digite a busca (1 - CPF; 2 - Telefone): ")
+
+#         if busca == '1':
+#             print("Ok")
+#             # if cpf in dados_pessoas:
+#             #     detalhes = dados_pessoas[cpf]
+#             #     print(f"Nome: {detalhes["Nome"]}")
+#             #     print(f"CPF: {detalhes["CPF"]}")
+#             #     print(f"RG: {detalhes["RG"]}")
+#             #     print(f"E-mail: {detalhes["E-mail"]}")
+#             #     print(f"Endereco: {detalhes["Endereco"]}")
+#             # else:
+#             #     print(f"A pessoa com o nome {cpf} não foi encontrada.")
+#         elif busca == '2':
+#             print("Ok dnv")
+
+
+# def listar_dados():
+#   print()
+#   print("Listagem das pessoas cadastradas:")
+#   for dado in dados_pessoas:
+#       print (dado)
+    
+# while True:
+#     print("\n--- Menu ---")
+#     print("1. Adicionar pessoas")
+#     print("2. Buscar dados")
+#     print("3. Listar pessoas")
+#     print("4. Sair")
+#     opcao = input("Escolha uma opção: ")
+#     print()
+
+#     if opcao =='1':
+#         adicionar_pessoas()
+
+#     elif opcao == '2':
+#         buscar_dados()
+
+#     elif opcao == '3':
+#         listar_dados()
+
+#     elif opcao == '4':
+#         print("ATENÇÃO! OS DADOS FORAM GRAVADOS")
+#         print("PROGRAMA FINALIZADO...")
+#         break
+#     else:
+#         print("Opção inválida.")
