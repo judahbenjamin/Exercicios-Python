0784fb5f534df847536317a1988dc070bd47938a
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-10-14 17:03:44 -0300

    Add files via upload

---
+++ exercicio1_oper_strings.py
@@ -0,0 +1,13 @@
+#EXERCÍCIO 1 DE OPERAÇÕES COM STRINGS
+#POR JUDAH BENJAMIN
+#DATA: 14/10/2024
+
+"""
+1. Crie um programa que deverá receber uma "palavra" e exibir cada caractere da "palavra" em uma linha.   
+"""
+
+# palavra = input("Digite um texto: ")
+# print("//// Acessando os caracteres do inicio ao fim ////")
+
+# for i in range(len(palavra)):
+#     print(palavra[i])
